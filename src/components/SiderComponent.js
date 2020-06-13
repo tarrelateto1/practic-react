ef0d6d4e22999971d802d78055c2c373d132a2b5
tar <jirawat.tar@g.swu.ac.th>
2020-06-13 20:01:06 +0700

    Adjust SiderComponent

--- src/components/SiderComponent.js
+++ src/components/SiderComponent.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
 import {Layout, Menu} from 'antd';
-import {UploadOutlined, UserOutlined, VideoCameraOutlined} from '@ant-design/icons';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined,
+  } from '@ant-design/icons';
 
 const {  Sider } = Layout;
+const { SubMenu } = Menu;
 
 class SiderComponent extends Component{
     render(){
@@ -18,20 +25,23 @@ class SiderComponent extends Component{
                 }}
             >
                 <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                        nav 1
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                        nav 2
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<UploadOutlined />}>
-                        nav 3
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />}>
-                        nav 4
-                    </Menu.Item>
-                </Menu>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+          </Menu>
             </Sider>
         )
     }
