04f629f68e00a1033a619d0534effed0ebe418d4
Chanon Treemeth <chanon29060@live.com>
2020-06-14 19:18:37 +0700

    Update Detail content in project

--- src/containers/Contents/Clip/ClipClap/index.js
+++ src/containers/Contents/Clip/ClipClap/index.js
@@ -12,6 +12,15 @@ export default class ClipClap extends Component {
                     <Row justify="start" >
                         <Col sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 6 }} xl={{ span: 6 }} >
                         <h1>ClipClap</h1>
+                        <p>วิธีในการถอนเงิน : Paypal</p>
+                        <p>คำอธิบายแอพริเคชั่น : เป็นแอพดูคลิปตลกเพื่อรับเหรียย</p>
+                        <p>ลิ้งโหลดแอพ : http</p>
+                        <h3>วิธีการสมัครงาน</h3>
+                        <p>รูปเรียงกัน </p>
+                        <h3>โค้ดรับเงิน</h3>
+                        <p>รูปเรียงกัน </p>
+                        <h3>วิธีแลกเงิน</h3>
+                        <p>รูปเรียงกัน </p>
                         </Col>
                     </Row>
                 </div>
--- src/containers/Contents/Game/CoinFarm/index.js
+++ src/containers/Contents/Game/CoinFarm/index.js
@@ -1,10 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+import {Layout,Row,Col} from "antd"
+
+const {Content}=Layout
 export default class CoinFarm extends Component {
     render(){
         return (
-            <div>
-                CoinFarm
+            <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360, margin: '24px 16px 0' }}>
+                <Row justify="start" >
+                    <Col sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 6 }} xl={{ span: 6 }} >
+                    <h1>ClipClap</h1>
+                    <p>วิธีในการถอนเงิน : Paypal</p>
+                    <p>คำอธิบายแอพริเคชั่น : เป็นแอพดูคลิปตลกเพื่อรับเหรียย</p>
+                    <p>ลิ้งโหลดแอพ : http</p>
+                    <h3>วิธีการสมัครงาน</h3>
+                    <p>รูปเรียงกัน </p>
+                    <h3>โค้ดรับเงิน</h3>
+                    <p>รูปเรียงกัน </p>
+                    <h3>วิธีแลกเงิน</h3>
+                    <p>รูปเรียงกัน </p>
+                    </Col>
+                </Row>
             </div>
+        </Content>
         )
     }
     }
