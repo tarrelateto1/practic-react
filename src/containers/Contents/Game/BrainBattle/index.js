7cf6721d26a288aa98746a1dfb1024d743ef4da9
tar <jirawat.tar@g.swu.ac.th>
2020-06-21 16:10:56 +0700

    Add BrainBatte Page

--- src/containers/Contents/Game/BrainBattle/index.js
+++ src/containers/Contents/Game/BrainBattle/index.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import {Layout,Row,Col} from "antd"
 
+//register image
+import register1 from '../../../../assets/ฺBrainBattle/register/register1.jpeg'
+
+//code image
+import code1 from '../../../../assets/ฺBrainBattle/moneyCode/code1.jpeg'
+import code2 from '../../../../assets/ฺBrainBattle/moneyCode/code2.jpeg'
+import code3 from '../../../../assets/ฺBrainBattle/moneyCode/code3.jpeg'
+
+//redeem image
+import redeem1 from '../../../../assets/ฺBrainBattle/redeem/redeem1.jpeg'
+import redeem2 from '../../../../assets/ฺBrainBattle/redeem/redeem2.jpeg'
+import code4 from "../../../../assets/Clip/ClipClap/moneyCode/code4.jpeg";
+import code5 from "../../../../assets/Clip/ClipClap/moneyCode/code5.jpeg";
+import redeem3 from "../../../../assets/Clip/ClipClap/redeem/redeem3.jpeg";
+
+
 const {Content}=Layout
 
 export default class BrainBattle extends Component {
@@ -12,14 +28,123 @@ render(){
                 <Col sm={{ span: 12 }} md={{ span: 12 }} lg={{ span: 6 }} xl={{ span: 6 }} >
                 <h1>BrainBattle</h1>
                 <p>วิธีในการถอนเงิน : Paypal</p>
-                <p>คำอธิบายแอพริเคชั่น : เป็นแอพดูคลิปตลกเพื่อรับเหรียย</p>
-                <p>ลิ้งโหลดแอพ : http</p>
-                <h3>วิธีการสมัครงาน</h3>
-                <p>รูปเรียงกัน </p>
-                <h3>โค้ดรับเงิน</h3>
-                <p>รูปเรียงกัน </p>
-                <h3>วิธีแลกเงิน</h3>
-                <p>รูปเรียงกัน </p>
+                <p>คำอธิบายแอพริเคชั่น : เป็นการคิดเลขเก็บเงิน</p>
+                    <p>รองรับโทรศัพท์ : ทั้ง Android และ IOS</p>
+                {/*<p>รูปเรียงกัน </p>*/}
+                {/*<h3>โค้ดรับเงิน</h3>*/}
+                {/*<p>รูปเรียงกัน </p>*/}
+                {/*<h3>วิธีแลกเงิน</h3>*/}
+                {/*<p>รูปเรียงกัน </p>*/}
+                </Col>
+            </Row>
+            <Row justify="center">
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 12}} xl={{span: 12}}>
+                    <div className="ct">
+                        <h2>วิธีการสมัครงาน</h2>
+                    </div>
+                </Col>
+            </Row>
+            <Row justify="center">
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 12}} xl={{span: 12}}>
+                    <div className="ct">
+                        <img src={register1} alt="register1"/>
+                    </div>
+                </Col>
+            </Row>
+            <Row justify="center">
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 6}} xl={{span: 6}}>
+                    <div className="ct">
+                        <h2>โค้ดรับเงิน</h2>
+                    </div>
+                </Col>
+            </Row>
+            <Row justify="start">
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 8}} xl={{span: 8}}>
+                    <div className="bd-rad">
+
+                        <Row justify="center">
+                            <Col span={2}>
+                                <h2>1</h2>
+                            </Col>
+                        </Row>
+                        <Row justify="center">
+                            <Col span={24}>
+                                <img src={code1} alt="code1"/>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 8}} xl={{span: 8}}>
+                    <div className="bd-rad">
+
+                        <Row justify="center">
+                            <Col span={2}>
+                                <h2>2</h2>
+
+                            </Col>
+                        </Row>
+                        <Row justify="center">
+                            <Col span={24}>
+                                <img src={code2} alt="code2"/>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 8}} xl={{span: 8}}>
+                    <div className="bd-rad">
+
+                        <Row justify="center">
+                            <Col span={2}>
+                                <h2>3</h2>
+                            </Col>
+                        </Row>
+                        <Row justify="center">
+                            <Col span={24}>
+                                <img src={code3} alt="code3"/>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+            </Row>
+
+            <Row justify="center">
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 6}} xl={{span: 6}}>
+                    <div className={"ct"}>
+                        <h2>วิธีแลกเงิน</h2>
+                    </div>
+                </Col>
+            </Row>
+            <Row justify="start">
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 8}} xl={{span: 8}}>
+                    <div className="bd-rad">
+
+                        <Row justify="center">
+                            <Col span={2}>
+                                <h2>1</h2>
+                            </Col>
+                        </Row>
+                        <Row justify="center">
+                            <Col span={24}>
+                                <img src={redeem1} alt="redeem1"/>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+                <Col sm={{span: 12}} md={{span: 12}} lg={{span: 8}} xl={{span: 8}}>
+                    <div className="bd-rad">
+
+                        <Row justify="center">
+                            <Col span={2}>
+                                <h2>2</h2>
+
+                            </Col>
+                        </Row>
+                        <Row justify="center">
+                            <Col span={24}>
+                                <img src={redeem2} alt="redeem2"/>
+                            </Col>
+                        </Row>
+                    </div>
                 </Col>
             </Row>
         </div>
