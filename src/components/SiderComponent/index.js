2c5e0f8e656d6aa56f85850bea0c36e59290fb33
tar <jirawat.tar@g.swu.ac.th>
2020-07-06 21:01:26 +0700

    Adjust Menu

--- src/components/SiderComponent/index.js
+++ src/components/SiderComponent/index.js
@@ -47,55 +47,37 @@ class SiderComponent extends Component {
                             <Link to="/bitcoin/freebitco"/>
                         </Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub5" icon={<TeamOutlined />} title="กระเป๋าเงินออนไลน์">
-                        <Menu.Item key="7">Paypal
-                            <Link to="/wallet/paypal"/>
-                        </Menu.Item>
-                        <Menu.Item key="8">Bitkub
-                            <Link to="/wallet/bitkub"/>
-                        </Menu.Item>
-                    </SubMenu>
-                </Menu>
-                <Menu theme="dark" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline">
-                    <SubMenu key="sub1" title={
-                            <span>
-              <MailOutlined />
-              <span>Navigation One</span>
-            </span>
-                        }
-                    >
-                        <Menu.ItemGroup key="g1" title="Item 1">
-                            <Menu.Item key="1">Option 1</Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
+                    {/*<SubMenu key="sub5" icon={<TeamOutlined />} title="กระเป๋าเงินออนไลน์">*/}
+                    {/*    <Menu.Item key="7">Paypal*/}
+                    {/*        <Link to="/wallet/paypal"/>*/}
+                    {/*    </Menu.Item>*/}
+                    {/*    <Menu.Item key="8">Bitkub*/}
+                    {/*        <Link to="/wallet/bitkub"/>*/}
+                    {/*    </Menu.Item>*/}
+                    {/*</SubMenu>*/}
+                    <SubMenu key="sub5" title={
+                        <span><MailOutlined /><span>กระเป๋าเงินออนไลน์</span></span>}>
+                        <Menu.ItemGroup key="g1" title="Paypal">
+                            <Menu.Item key="ึ7">สมัคร        <Link to="/wallet/paypal"/></Menu.Item>
+                            {/*<Menu.Item key="8">ถอน</Menu.Item>*/}
                         </Menu.ItemGroup>
-                        <Menu.ItemGroup key="g2" title="Item 2">
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>
+                        <Menu.ItemGroup key="g2" title="bitkub">
+                            <Menu.Item key="9">สมัคร<Link to="/wallet/bitkub"/></Menu.Item>
+                            {/*<Menu.Item key="10">ถอน </Menu.Item>*/}
                         </Menu.ItemGroup>
                     </SubMenu>
-                    <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub4"
-                        title={
-                            <span>
-              <SettingOutlined />
-              <span>Navigation Three</span>
-            </span>
-                        }
-                    >
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <Menu.Item key="11">Option 11</Menu.Item>
-                        <Menu.Item key="12">Option 12</Menu.Item>
-                    </SubMenu>
+
+                    {/*<SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">*/}
+                    {/*    <Menu.Item key="5">Option 5</Menu.Item>*/}
+                    {/*    <Menu.Item key="6">Option 6</Menu.Item>*/}
+                    {/*    <SubMenu key="sub3" title="Submenu">*/}
+                    {/*        <Menu.Item key="7">Option 7</Menu.Item>*/}
+                    {/*        <Menu.Item key="8">Option 8</Menu.Item>*/}
+                    {/*    </SubMenu>*/}
+                    {/*</SubMenu>*/}
                 </Menu>
+
+
             </Sider>
         )
     }
